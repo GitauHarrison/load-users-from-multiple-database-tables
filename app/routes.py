e81ffaa25d1e1ad73421fe330ed0c194d568670d
harry <harrisongitau3@gmail.com>
2021-12-18 19:38:10 +0300

    Dev: Add registration functionality

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,29 @@
 from app import app
 from flask import render_template
+from app.forms import RegisterForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register/student')
+def register_student():
+    form = RegisterForm()
+    return render_template(
+        'register.html',
+        title='Register Student',
+        form=form
+        )
+
+
+@app.route('/register/teacher', methods=['GET', 'POST'])
+def register_teacher():
+    form = RegisterForm()
+    return render_template(
+        'register.html',
+        title='Register Teacher',
+        form=form
+        )
